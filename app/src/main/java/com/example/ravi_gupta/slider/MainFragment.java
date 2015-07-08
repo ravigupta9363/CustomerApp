bcee03ba8ff916eda93291540e7f9944e984c4cc
ravi gupta <ravigupta9363@gmail.com>
2015-07-08 14:28:55 +0530

    "Location of customer app has been completed successfully"

---
+++ app/src/main/java/com/example/ravi_gupta/slider/MainFragment.java
@@ -0,0 +1,312 @@
+package com.example.ravi_gupta.slider;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainFragment extends android.support.v4.app.Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    ViewPagerCustomDuration viewPager;
+    PagerAdapter pagerAdapter;
+    int[] sliderItems;
+    Timer timer;
+    String newAddress;
+    String address1;
+    String address2;
+    int page = 1;
+    AppLocationService appLocationService;
+    EditText disabledocationEditText;
+    boolean isItemClickable;
+    MainActivity mainActivity;
+    public static String TAG = "MainFragment";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+
+    // TODO: Rename and change types and number of parameters
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+
+        sliderItems = new int[]{R.drawable.image1, R.drawable.image1, R.drawable.image1, R.drawable.image1, R.drawable.image1};
+        pagerAdapter = new ViewPagerAdapter(getActivity(), sliderItems, viewPager);
+        viewPager = (ViewPagerCustomDuration) rootview.findViewById(R.id.viewPager);
+        viewPager.setScrollDurationFactor(0.5);
+        viewPager.setPadding(40, 0, 40, 0);
+        viewPager.setClipToPadding(false);
+        viewPager.setPageMargin(10);
+        //http://stackoverflow.com/questions/7395655/set-default-page-for-viewpager-in-android
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setCurrentItem(1, false);
+        viewPager.setScrollDurationFactor(3);
+        pageSwitcher(4);
+
+        disabledocationEditText = (EditText) rootview.findViewById(R.id.fragment_main_edittext1);
+
+        appLocationService = new AppLocationService(getActivity());
+        Location gpsLocation = appLocationService
+                .getLocation(LocationManager.NETWORK_PROVIDER);
+        if (gpsLocation != null) {
+            double latitude = gpsLocation.getLatitude();
+            double longitude = gpsLocation.getLongitude();
+            // String result = "Latitude: " + gpsLocation.getLatitude() +
+            //    " Longitude: " + gpsLocation.getLongitude();
+            //locationEdittext.setText(result);
+        } else {
+            showSettingsAlert();
+        }
+
+        Location location = appLocationService
+                .getLocation(LocationManager.NETWORK_PROVIDER);
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            LocationAddress locationAddress = new LocationAddress();
+            locationAddress.getAddressFromLocation(latitude, longitude,
+                    getActivity().getApplicationContext(), new GeocoderHandler());
+        } else {
+            showSettingsAlert();
+        }
+
+        disabledocationEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.replaceFragment(R.id.fragment_main_edittext1, null);
+                Log.v("Result","Called");
+            }
+        });
+        return rootview;
+    }
+
+    public void pageSwitcher(int seconds) {
+        timer = new Timer(); // At this line a new Thread will be created
+        timer.scheduleAtFixedRate(new RemindTask(), 0, seconds * 1000); // delay
+        // in
+        // milliseconds
+    }
+
+
+    // this is an inner class...
+    class RemindTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            // As the TimerTask run on a seprate thread from UI thread we have
+            // to call runOnUiThread to do work on UI thread.
+            getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+
+                    if (page > 9999) { // In my case the number of pages are 5
+                        timer.cancel();
+                        // Showing a toast for just testing purpose
+                    } else {
+                        if (page == 0) {
+                            viewPager.setCurrentItem(3);
+                            Log.v("dc", "dc  1");
+                        }
+
+                        // skip fake page (last), go to first page
+                        if (page == 4) {
+                            Log.v("dc", "dc  2");
+                            viewPager.setCurrentItem(0);
+                            //notice how this jumps to position 1, and not position 0. Position 0 is the fake page!
+                        } else {
+                            Log.v("dc", "dc  3");
+                            viewPager.setCurrentItem(page++);
+                            if (page == 4)
+                                page = 1;
+                        }
+                    }
+                }
+            });
+
+        }
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                getActivity());
+        alertDialog.setTitle("SETTINGS");
+        alertDialog.setMessage("Enable Location Provider! Go to settings menu?");
+        alertDialog.setPositiveButton("Settings",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        getActivity().startActivity(intent);
+                    }
+                });
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private class GeocoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            Log.v("Result",mainActivity.updateLocation+"");
+            switch (mainActivity.updateLocation) {
+
+                case 0 :  disabledocationEditText.setText(locationAddress);
+                    break;
+                case 1 : newAddress = getArguments().getString("newAddress");
+                        disabledocationEditText.setText(newAddress);
+                    break;
+                case 2 : address1 = getArguments().getString("newAddress1");
+                         address2 = getArguments().getString("newAddress2");
+                         disabledocationEditText.setText(address1+", "+address2);
+                    break;
+                case 3 : disabledocationEditText.setText(locationAddress);
+                    break;
+            }
+
+        }
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mainActivity = (MainActivity) getActivity();
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    private void hiddenKeyboard(View v) {
+        InputMethodManager keyboard = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        keyboard.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        new AsyncCaller().execute();
+    }
+
+    private class AsyncCaller extends AsyncTask<Void, Void, Void>
+    {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //this method will be running on UI thread
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            //this method will be running on background thread so don't update UI frome here
+            //do your long running http tasks here,you dont want to pass argument and u can access the parent class' variable url over here
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            //this method will be running on UI thread
+        }
+
+    }
+
+
+
+}
