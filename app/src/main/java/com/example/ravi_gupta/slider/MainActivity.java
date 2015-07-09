417459399e891a48cfd43cb11e63ede948b7d478
ravi gupta <ravigupta9363@gmail.com>
2015-07-10 01:22:09 +0530

    "Action Bar has been Completed Phase 2"

--- app/src/main/java/com/example/ravi_gupta/slider/MainActivity.java
+++ app/src/main/java/com/example/ravi_gupta/slider/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Spannable;
@@ -24,6 +25,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -341,7 +343,19 @@ public class MainActivity extends ActionBarActivity implements ListFragment.OnFr
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuItem item = menu.findItem(R.id.cart);
+        MenuItemCompat.setActionView(item, R.layout.shopping_cart);
+        ImageButton notifCount = (ImageButton) MenuItemCompat.getActionView(item);
+        notifCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("menu","Cart");
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
+
+        //return true;
     }
 
 
