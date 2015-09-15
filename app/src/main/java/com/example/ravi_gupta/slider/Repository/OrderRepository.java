6256ef045d1dedbec90fc7efb0d4508abbe461dd
ravi gupta <ravigupta9363@gmail.com>
2015-09-15 12:36:38 +0530

    "Status  is been changed 2"

--- app/src/main/java/com/example/ravi_gupta/slider/Repository/OrderRepository.java
+++ app/src/main/java/com/example/ravi_gupta/slider/Repository/OrderRepository.java
@@ -1,12 +1,7 @@
 package com.example.ravi_gupta.slider.Repository;
 
 import com.example.ravi_gupta.slider.Models.Order;
-import com.google.common.collect.ImmutableMap;
 import com.strongloop.android.loopback.ModelRepository;
-import com.strongloop.android.loopback.callbacks.VoidCallback;
-import com.strongloop.android.remoting.adapters.Adapter;
-import com.strongloop.android.remoting.adapters.RestContract;
-import com.strongloop.android.remoting.adapters.RestContractItem;
 
 /**
  * Created by Ravi-Gupta on 8/13/2015.
@@ -15,6 +10,9 @@ public class OrderRepository extends ModelRepository<Order> {
     public OrderRepository() {
         super("Order", null, Order.class);
     }
+
+
+  
 
 
 }
